7ac08b8ce51367425f7ae81b8d098d4e0936c5f7
Guillaume <guillaume.fuhr@univ-provence.fr>
2012-01-05 16:45:35 +0100

    implicit scheme

--- TP2/include/H2D.h
+++
@@ -1,8 +0,0 @@
-
-
-#ifndef H2D_H
-#define H2D_H
-#include "./common.h"
-#include "./H2D_Func.h"
-#include "./H2D_types.h"
-#endif
--- TP2/include/schemes.h
+++
@@ -1,8 +0,0 @@
-#ifndef SCHEMES_H
-#define SCHEMES_H
-
-int RK4(double *const Up1, double const*const U, sConst const*const psc,pfLinearOp pfTimeStep, pfBC pfBoundary);
-int eule(double *const Up1, double const*const U, sConst const*const psc,pfLinearOp pfTimeStep, pfBC pfBoundary);
-
-
-#endif
